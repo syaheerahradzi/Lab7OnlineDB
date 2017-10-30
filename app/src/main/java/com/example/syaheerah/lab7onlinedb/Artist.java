c28412ef05366f2d970d05945d5722a6a1cd5306
Student4 <student5@iMac4s-iMac.local>
2017-10-30 09:39:45 +0800

    Initial commit

---
+++ app/src/main/java/com/example/syaheerah/lab7onlinedb/Artist.java
@@ -0,0 +1,24 @@
+package com.example.syaheerah.lab7onlinedb;
+
+
+
+public class Artist {
+
+    String artistId;
+    String artistName;
+
+    public Artist() {}
+
+    public Artist(String artistId, String artistName) {
+        this.artistId = artistId;
+        this.artistName = artistName;
+    }
+
+    public String getArtistId() {
+        return artistId;
+    }
+
+    public String getArtistName() {
+        return artistName;
+    }
+}
